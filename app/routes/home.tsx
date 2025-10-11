bdc04caed632403dd0232ad7a75fd92d1712367d
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2025-10-11 23:38:39 +0530

    header , nav bar, and hero section
    created hero section header and nav bar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ai Resume Analyzer" },
+    { name: "description", content: "Get your resume better with me" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+  <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Review your submission and check AI-powered feedback</h2>
+      </div>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resume-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+  </main>
 }
